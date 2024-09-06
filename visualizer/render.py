17cd9a4c32d006417f7eaa613556e00275938bb5
Tim Goodwin <timg.goodwin@gmail.com>
2024-09-05 17:38:09 -0700

    better color assignment

--- visualizer/render.py
+++ visualizer/render.py
@@ -4,20 +4,44 @@ import matplotlib.pyplot as plt
 from dataclasses import dataclass
 from collections import defaultdict
 import uuid
-
-pastel_rainbow_palette = [
-    "#FFB3BA",  # Pastel Red
-    "#FFDFBA",  # Pastel Orange
-    "#FFFFBA",  # Pastel Yellow
-    "#BAFFC9",  # Pastel Green
-    "#BAE1FF",  # Pastel Blue
-    "#D1BAFF",  # Pastel Purple
-    "#FFBAE1",  # Pastel Pink
-]
+import colorsys
 
 
-def get_a_color(id, palette=pastel_rainbow_palette):
-    return palette[hash(id) % len(palette)]
+def generate_pastel_colors(num_colors):
+    """
+    Generates a list of pastel hex colors evenly spaced around the color wheel.
+
+    Parameters:
+    num_colors (int): The number of distinct pastel colors to generate.
+
+    Returns:
+    list: A list of hex color codes.
+    """
+    pastel_colors = []
+
+    # Evenly space hues around the color wheel
+    for i in range(num_colors):
+        hue = i / num_colors  # Evenly spaced hues
+        # Use high value and low-to-medium saturation to get pastel-like colors
+        rgb = colorsys.hsv_to_rgb(hue, 0.4, 1.0)  # S = 0.4 for pastel, V = 1.0 for brightness
+        # Convert to hex
+        pastel_colors.append('#{:02x}{:02x}{:02x}'.format(int(rgb[0] * 255), int(rgb[1] * 255), int(rgb[2] * 255)))
+
+    return pastel_colors
+
+def assign_colors_to_ids(ids):
+    """
+    Assigns a unique pastel color to each ID.
+
+    Parameters:
+    ids (list): A list of unique IDs.
+
+    Returns:
+    dict: A dictionary mapping each ID to a unique pastel color.
+    """
+    num_ids = len(ids)
+    pastel_colors = generate_pastel_colors(num_ids)
+    return {id_: color for id_, color in zip(ids, pastel_colors)}
 
 
 @dataclass
@@ -55,14 +79,12 @@ class Event:
 class LamportDiagram:
     def __init__(self):
         # Maps process_id to a list of events
-        self.processes = {}
+        self.processes = defaultdict(list)
         self.events = []
 
         self.events_by_id = {}
 
     def add_event(self, event):
-        if event.process_id not in self.processes:
-            self.processes[event.process_id] = []
         self.processes[event.process_id].append(event)
         self.events.append(event)
         self.events_by_id[event.id] = event
@@ -81,7 +103,7 @@ class LamportDiagram:
         for i, process_id in enumerate(sorted(self.processes.keys())):
             process_y_positions[process_id] = i
             ax.plot([0, max_x], [i, i], 'k-')  # Draw a horizontal line for each process
-            ax.text(-0.5, i, f"Process {process_id}", verticalalignment='center')
+            ax.text(-0.5, i + 0.1, f"Process {process_id}", verticalalignment='center')
 
         # Drawing events
         for event in sorted(self.events, key=lambda e: e.timestamp):
@@ -89,35 +111,49 @@ class LamportDiagram:
             y = process_y_positions[event.process_id]
             events_by_reconcile_id[event.reconcile_id].append(event)
             ax.plot(x, y, 'bo', zorder=1)  # Plot event as a dot
-            ax.text(x + 0.1, y, f"Event {event.timestamp}", verticalalignment='center')
+            ax.text(x + 0.1, y + 0.1, f"Event {event.timestamp}", verticalalignment='center')
 
             # Drawing arrows for communication
+            # TODO handle case when GET event is on the same process but is not the most recent value
             if event.is_send_event():
                 receiver_y = process_y_positions[self.events_by_id[event.receiver_id].process_id]
-                ax.arrow(x, y, 2, receiver_y - y, head_width=0.2, head_length=0.3, fc='r', ec='r', zorder=2)
+                receiver_x = event_x_positions[event.receiver_id]
+                ax.arrow(x, y, receiver_x - x, receiver_y - y, head_width=0.1, head_length=0.1, fc='r', ec='r', zorder=2)
             elif event.is_receive_event():
                 sender_y = process_y_positions[self.events_by_id[event.sender_id].process_id]
-                ax.arrow(x - 2, sender_y, 2, y - sender_y, head_width=0.2, head_length=0.3, fc='r', ec='r', zorder=2)
+                sender_x = event_x_positions[event.sender_id]
+                ax.arrow(sender_x, sender_y, x - sender_x, y - sender_y, head_width=0.1, head_length=0.1, fc='r', ec='r', zorder=2)
 
+        reconcile_id_color = assign_colors_to_ids(set(event.reconcile_id for event in self.events))
         for reconcile_id, events in events_by_reconcile_id.items():
             process_id = events[0].process_id
             y_pos = process_y_positions[process_id]
             positions = [event_x_positions[e.id] for e in events]
             min_x = min(positions)
             max_x = max(positions)
+            # reconcile_id_color[reconcile_id] = get_a_color(reconcile_id)
             plt.gca().add_patch(
                 plt.Rectangle(
                     (min_x - 0.2, y_pos - 0.2),
                     max_x - min_x + 0.4, 0.4,
-                    color=get_a_color(reconcile_id),
+                    color=reconcile_id_color[reconcile_id],
                     edgecolor='g',
                     zorder=0)
                 )
 
 
+
+        # Create legend for reconcile_id colors
+        legend_elements = []
+        for reconcile_id, color in reconcile_id_color.items():
+            legend_elements.append(plt.Line2D([0], [0], marker='o', color='w', label=f"Reconcile {reconcile_id}", markerfacecolor=color, markersize=10))
+        ax.legend(handles=legend_elements, loc='upper right')
+
+        # Set axis limits
         ax.set_xlim(-1, max_x)
         ax.set_ylim(-1, len(self.processes))
         ax.set_yticks([])
+
         plt.show()
 
 
