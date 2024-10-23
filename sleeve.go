5957691b103072f1a92cc584ebde08d9aed60e89
Tim Goodwin <timg.goodwin@gmail.com>
2024-10-23 16:09:50 -0700

    externalize top level configs

--- sleeve.go
+++ sleeve.go
@@ -1,10 +1,25 @@
 package sleeve
 
 import (
+	"time"
+
 	"github.com/tgoodwin/sleeve/pkg/client"
 	kclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
+func VisibilityDelay(kind string, duration time.Duration) client.Option {
+	return client.VisibilityDelay(kind, duration)
+}
+
+func TrackSnapshots() client.Option {
+	return func(o *client.Config) {
+		o.LogObjectSnapshots = true
+	}
+}
+
 func Wrap(wrapped kclient.Client) *client.Client {
+	return client.Wrap(wrapped)
+}
+func NewClient(wrapped kclient.Client) *client.Client {
 	return client.Wrap(wrapped)
 }
